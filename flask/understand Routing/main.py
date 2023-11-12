89146a183736791ae1a56a3fda65f4e99d92bc6a
Raed-93 <tbilhraed@gmail.com>
2023-11-12 21:48:50 +0200

    new flask

--- flask/understand Routing/main.py
+++ flask/understand Routing/main.py
@@ -1,10 +1,10 @@
-# from flask import Flask  # Import Flask to allow us to create our app
-# app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-# @app.route('/')          # The "@" decorator associates this route with the function immediately following
+# from flask import Flask  
+# app = Flask(__name__)    
+# @app.route('/')          
 # def hello_world():
-#     return 'Hello World!'  # Return the string 'Hello World!' as a response
-# if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-#  app.run(debug=True)    # Run the app in debug mode.
+#     return 'Hello World!'  
+# if __name__=="__main__":     
+#  app.run(debug=True)    
 
 #2
 
@@ -29,19 +29,16 @@
 #  app.run(debug=True)    
 
 
+#4
+# from flask import Flask
+# app=Flask(__name__)
 
+# @app.route('/repeat/<int:times>/<word>')
+# def repeat_word(times, word):
+#     return f'{word} ' * times
 
-
-
-
-
-
-from flask import Flask
-app=Flask(__name__)
-@app.route('/repeat/<int:times>/<word>')
-def repeat_word(times, word):
-    return f'{word} ' * times
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
  
+
+
